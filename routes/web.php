7e8cb01b74ef1d3298a552b8f2582fb86c121cd9
ezechieldef <ezechielahouandjinou@gmail.com>
2025-08-13 22:03:11 +0100

    ...

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\PlanController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -12,9 +12,11 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\NotificationController;
 use App\Http\Controllers\SessionSignalController;
+use App\Http\Controllers\SignalController;
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -28,7 +30,9 @@ Route::middleware('auth')->group(function () {
     Route::resource('users', UserController::class)->except(['edit']);
 
     Route::resource('session-signals', SessionSignalController::class);
-
+    Route::resource('plans', PlanController::class);
+    Route::resource('signals', SignalController::class);
 });
 
 require __DIR__ . '/auth.php';
+require __DIR__ . '/stone.php';
