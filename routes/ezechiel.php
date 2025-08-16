89b51c9ee49de66638323e2aaf70da4be456f1e0
ezechieldef <ezechielahouandjinou@gmail.com>
2025-08-16 13:36:43 +0100

    ...

--- routes/ezechiel.php
+++ routes/ezechiel.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\ActifController;
 use App\Http\Controllers\SignalController;
 use App\Http\Middleware\AccederAuxSignaux;
